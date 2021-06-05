c061e80a0c0638a9614b12583a6052216995ff6a
RAHULPATEKAR <rahulgpatekar@gmail.com>
2021-06-06 00:50:15 +0530

    custom_bone

---
+++ bones_dataset/bone_to_coco.py
@@ -0,0 +1,141 @@
+# %load shapes_to_coco.py
+#!/usr/bin/env python3
+
+import datetime
+import json
+import os
+import re
+import fnmatch
+from PIL import Image
+import numpy as np
+from pycococreatortools import pycococreatortools
+
+
+ROOT_DIR = 'D:/pycococreator-master/examples_1/bones/train2'
+IMAGE_DIR = os.path.join(ROOT_DIR, "bone_train_2019")
+ANNOTATION_DIR = os.path.join(ROOT_DIR, "annotations_2019")
+INFO = {
+    "description": "knee Dataset",
+    "url": "https://github.com/waspinator/pycococreator",
+    "version": "0.1.0",
+    "year": 2019,
+    "contributor": "waspinator",
+    "date_created": datetime.datetime.utcnow().isoformat(' ')
+}
+
+LICENSES = [
+    {
+        "id": 1,
+        "name": "Attribution-NonCommercial-ShareAlike License",
+        "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
+    }
+]
+
+CATEGORIES = [
+    {
+        'id': 1,
+        'name': 'Femur',
+        'supercategory': 'bone',
+    },
+    {
+        'id': 2,
+        'name': 'Tibia',
+        'supercategory': 'bone',
+    },
+    {
+        'id': 3,
+        'name': 'Patella',
+        'supercategory': 'bone',
+    },
+]
+
+def filter_for_jpeg(root, files):
+    file_types = ['*.jpeg', '*.jpg']
+    file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
+    files = [os.path.join(root, f) for f in files]
+    files = [f for f in files if re.match(file_types, f)]
+    
+    return files
+
+def filter_for_annotations(root, files, image_filename):
+    file_types = ['*.png']
+#    print(f'..................>:{file_types}')
+    file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
+#    print(f'...............>:{file_types}')
+    basename_no_extension = os.path.splitext(os.path.basename(image_filename))[0]
+#    print(f'basename_no_extension: {basename_no_extension}')
+    file_name_prefix = basename_no_extension #+ '.*'
+#    print(f'file_name_prefix: {file_name_prefix[0:]}')
+    files = [os.path.join(root, f) for f in files]
+#    print(f'files :{files}')
+    files = [f for f in files if re.match(file_types, f)]
+#    print(f'file re match:{files}')
+    files = [f for f in files if file_name_prefix in f]
+#    print(f'\nall annotation files:{files}')
+    return files
+
+def main():
+
+    coco_output = {
+        "info": INFO,
+        "licenses": LICENSES,
+        "categories": CATEGORIES,
+        "images": [],
+        "annotations": []
+    }
+
+    image_id = 1
+    segmentation_id = 1
+    
+    # filter for jpeg images
+    for root, _, files in os.walk(IMAGE_DIR):
+        image_files = filter_for_jpeg(root, files)
+
+        # go through each image
+        for image_filename in image_files:
+            image = Image.open(image_filename)
+            print('\n')
+            print(f'\nimage file: {image_filename}')
+
+
+            image_info = pycococreatortools.create_image_info(
+                image_id, os.path.basename(image_filename), image.size)
+            coco_output["images"].append(image_info)
+
+            # filter for associated png annotations
+            for root, _, files in os.walk(ANNOTATION_DIR):
+#                print(root)
+                annotation_files = filter_for_annotations(root, files, image_filename)
+#                print(annotation_files)
+                # go through each associated annotation
+                for annotation_filename in annotation_files:
+                    
+                    print(f'annotation_files: {annotation_filename}')
+                    class_id = [x['id'] for x in CATEGORIES if x['name'] in annotation_filename][0]
+                    print(f'class_id: {class_id}')
+                    category_info = {'id': class_id, 'is_crowd': 'crowd' in image_filename}
+                    binary_mask = np.asarray(Image.open(annotation_filename)
+                        .convert('1')).astype(np.uint8)
+                    
+                    annotation_info = pycococreatortools.create_annotation_info(
+                        segmentation_id, image_id, category_info, binary_mask,
+                        image.size, tolerance=2)
+
+                    if annotation_info is not None:
+                        coco_output["annotations"].append(annotation_info)
+                    print(f'segmentation_id: {segmentation_id}')
+                    segmentation_id = segmentation_id + 1
+                    
+            print(f'image_id: {image_id}')
+            image_id = image_id + 1
+
+    with open('{}/bone_data.json'.format(ROOT_DIR), 'w') as output_json_file:
+        json.dump(coco_output, output_json_file)
+
+
+if __name__ == "__main__":
+    main()
+path_1 = str(ROOT_DIR)+'/bone_data.json'
+#path = 'D:/coco/examples/shapes/instances_shape_train2018.json'
+with open(path_1) as f:
+    data_1 = json.load(f)
